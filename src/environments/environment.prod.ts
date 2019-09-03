7a5c9ec5a3602e8e17548992f4c4dbdb9def7636
alisson <diane_10_96@hotmail.com>
2019-09-03 01:17:48 -0500

    add-components

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firestore:{
+    apiKey: "AIzaSyAuwvViZiVlmWZ2JNUwgjC3jqdl09FXLj4",
+    authDomain: "control-clientes-30ffc.firebaseapp.com",
+    databaseURL: "https://control-clientes-30ffc.firebaseio.com",
+    projectId: "control-clientes-30ffc",
+    storageBucket: "control-clientes-30ffc.appspot.com",
+    messagingSenderId: "845450306639"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firestore:{
+    apiKey: "AIzaSyAuwvViZiVlmWZ2JNUwgjC3jqdl09FXLj4",
+    authDomain: "control-clientes-30ffc.firebaseapp.com",
+    databaseURL: "https://control-clientes-30ffc.firebaseio.com",
+    projectId: "control-clientes-30ffc",
+    storageBucket: "control-clientes-30ffc.appspot.com",
+    messagingSenderId: "845450306639"
+  }
 };
 
 /*
